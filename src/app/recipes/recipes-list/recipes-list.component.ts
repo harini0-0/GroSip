8c24a02e562b5758c45f1558449f035812d3cbd1
harini0-0 <harinipri2001@gmail.com>
2021-10-22 13:04:08 +0530

    update components

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a Test', 'https://downshiftology.com/wp-content/uploads/2015/11/shakshuka-recipe-israel.jpg'),
+    new Recipe('A Test Recipe 2', 'This is simply a Test 2', 'https://downshiftology.com/wp-content/uploads/2015/11/shakshuka-recipe-israel.jpg')
+  ];
 
   constructor() { }
 
